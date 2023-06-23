d1072587c11507b21d6b3f31c0dff2186edbc714
clarameyhazlinda <clarameyhazlinda@mhs.mdp.ac.id>
2023-06-23 09:16:37 +0700

    Update SplashActivity.java

--- app/src/main/java/com/si6a/i_laundry/SplashActivity.java
+++ app/src/main/java/com/si6a/i_laundry/SplashActivity.java
@@ -2,13 +2,30 @@ package com.si6a.i_laundry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.si6a.i_laundry.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
